bf572f8e9dd331de61232be15af19129d7837940
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-08-21 20:12:20 -0400

    add Reviews component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Ratings />
+  <Reviews />
 </template>
 
 <script>
 import Ratings from './components/Ratings.vue'
+import Reviews from './components/Reviews.vue'
 
 export default {
   name: 'App',
   components: {
     Ratings,
+    Reviews,
   }
 }
 </script>
---
+++ src/components/Reviews.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="reviews-container">
+        <div v-for="review in reviews" :key="review.id" class="review-display">
+            <img :src="review.image" alt="Reviewer Profile Pic" class="review-image">
+            <p class="review-name">{{review.name}}
+                <span v-if="review.verifiedBuyer" class="review-verified-buyer">
+                    Verified Buyer
+                </span>
+            </p>
+            <p class="review-comment">{{review.comment}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'reviews-container',
+    data() {
+        return {
+            reviews: [
+                {
+                    id: 2001,
+                    name: 'Colton Smith',
+                    verifiedBuyer: true,
+                    image: require('@/assets/images/image-colton.jpg'),
+                    comment: `"We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"`,
+                },
+                {
+                    id: 2002,
+                    name: 'Irene Roberts',
+                    verifiedBuyer: true,
+                    image: require('@/assets/images/image-irene.jpg'),
+                    comment: `"Customer service is always excellent and very quick turn around. completely delighted with the simplicity of the purchase and the speed of delivery."`,
+                },
+                {
+                    id: 2003,
+                    name: 'Anne Wallace',
+                    verifiedBuyer: true,
+                    image: require('@/assets/images/image-anne.jpg'),
+                    comment: `"Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"`,
+                },
+            ],
+        }
+    },
+}
+</script>
