4cacb2288bdfb0b1a3c4f6da0b2c6786a0c86c7a
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-08-21 20:09:38 -0400

    add ratings component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Ratings />
 </template>
 
 <script>
-
+import Ratings from './components/Ratings.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Ratings,
   }
 }
 </script>
---
+++ src/components/Ratings.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="ratings-container">
+        <div v-for="rating in ratings"
+             :key="rating.id"
+             class="rating-display">
+             <div class="rating-stars">
+                <img v-for="index in rating.stars" :key="index" :src="starImage">
+             </div>
+            <p class="rating-desc">Rated {{ rating.stars }} Stars in {{ rating.rater }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ratings-container',
+    data() {
+        return {
+            starImage: require('@/assets/images/icon-star.svg'),
+            ratings: [ 
+                { id: 1001, stars: 5, rater: 'Reviews'},
+                { id: 1002, stars: 5, rater: 'Report Guru'},
+                { id: 1003, stars: 5, rater: 'BestTech'},
+            ],
+        }
+    },
+}
+</script>
