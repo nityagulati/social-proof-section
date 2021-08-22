c53bac85c08ff1565b8c96c2c2b0c6817aa8ded2
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-08-21 21:06:16 -0400

    add app data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <Ratings />
-  <Reviews />
-  <SiteFooter />
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+    <p class="message">{{ message }}</p>
+    <Ratings />
+    <Reviews />
+    <SiteFooter />
+  </div>
 </template>
 
 <script>
@@ -15,7 +19,13 @@ export default {
     Ratings,
     Reviews,
     SiteFooter,
-  }
+  },
+  data() {
+    return {
+      title: '10,000+ of our users love our products.',
+      message: 'We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.',
+    }
+  },
 }
 </script>
 
