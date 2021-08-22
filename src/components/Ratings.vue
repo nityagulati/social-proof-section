e8c71c8b9675e887ba633ac9a5e76c8ca4b96a9f
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-08-21 21:09:49 -0400

    add Ratings styles

--- src/components/Ratings.vue
+++ src/components/Ratings.vue
@@ -26,3 +26,66 @@ export default {
     },
 }
 </script>
+
+<style lang="scss">
+.ratings-container {
+    margin-top: 2.7em;
+
+  .rating-display {
+    background-color: $color--secondary;
+    padding: 1em;
+    margin-bottom: 2em;
+    border-radius: 8px;
+
+    .rating-stars > * {
+        margin: 0 0.5em;
+    }
+
+    .rating-desc {
+      font-weight: $font-weight--bold;
+      font-size: $font-size--small;
+      color: $color--primary;
+      margin-top: 1em;
+    }
+  }
+} 
+
+// responsive styles
+@include mq($width-sm) {
+    .ratings-container {
+        .rating-display {
+            margin: 2em;
+        }
+    }
+}
+
+@include mq($width-md) {
+    .ratings-container {
+        margin-top: 0;
+
+        .rating-display {
+            @include flex($justify-content:space-around, $align-items:baseline);
+            width: 400px;
+            max-width: 445px;
+            margin: 0;
+            margin-bottom: 2em;
+
+            &:nth-of-type(2) {
+                margin-left: 3em;
+            }
+
+            &:last-of-type {
+                margin-left: 6em;
+            }
+        }
+    }
+}
+
+@include mq($width-lg) {
+    .ratings-container {
+        margin-left: 2.7em;
+        min-width: 540px;
+        align-self: center;
+    }
+}  
+</style>
