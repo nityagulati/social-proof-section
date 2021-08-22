6bab170b57e377cf71be48f63b2da7cfc91b9e01
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-08-21 20:16:24 -0400

    add site footer attribution

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Ratings />
   <Reviews />
+  <SiteFooter />
 </template>
 
 <script>
 import Ratings from './components/Ratings.vue'
 import Reviews from './components/Reviews.vue'
+import SiteFooter from './components/SiteFooter.vue'
 
 export default {
   name: 'App',
   components: {
     Ratings,
     Reviews,
+    SiteFooter,
   }
 }
 </script>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="site-footer">
+        <p class="attribution">Challenge by
+            <a :href='frontendUrl' target="_blank">Frontend Mentor</a>. 
+            Coded by <a :href='myWebsite'>{{ myName }}</a>.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'site-footer',
+    data() {
+        return {
+            frontendUrl: 'https://www.frontendmentor.io?ref=challenge',
+            myWebsite: 'https://www.nityagulati.com',
+            myName: 'Nitya Gulati',
+        }
+    },
+}
+</script>
